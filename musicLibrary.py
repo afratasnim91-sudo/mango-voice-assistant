509cd64fbb249e2c0758154daf2c09d480c09a42
Afra Tasnim <afratasnim91@gmail.com>
2026-05-18 23:59:19 +0600

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "long distance love":"https://www.youtube.com/watch?v=sqJ2QhjBQaw",
+    "affection":"https://www.youtube.com/watch?v=5soixb2U6xM",
+    "keep on loving you":"https://www.youtube.com/watch?v=PDJPpG8e4n4",
+    "stay":"https://www.youtube.com/watch?v=kTJczUoc26U"
+}
